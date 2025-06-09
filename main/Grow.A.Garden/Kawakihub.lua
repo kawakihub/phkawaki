6d7605aa7b76f3b906be284eeab04f1afd74d8cb
kawakihub <etherealhub999@gmail.com>
2025-06-10 01:24:29 +0800

    Update Kawakihub.lua

--- main/Grow.A.Garden/Kawakihub.lua
+++ main/Grow.A.Garden/Kawakihub.lua
@@ -1,21 +1,17 @@
-game:GetService("StarterGui"):SetCore("SendNotification", {Title="Script loading...", Text="Made by Kawaki", Duration=5})
-
+game:GetService("StarterGui"):SetCore("SendNotification", {Title="Script loading...", Text="Made by Kawaki", Duration=10})
 
 local l = loadstring(game:HttpGet("https://raw.githubusercontent.com/discoart/FluentPlus/refs/heads/main/release.lua", true))()
 local Window = l:CreateWindow({
-    Title = "Kawaki Hub",
-    SubTitle = "https://discord.gg/49DAzBrkX4",
-    TabWidth = 15,
+    Title = "",
+    SubTitle = "",
+    TabWidth = 0,
     Size = UDim2.fromOffset(0, 0),
-    Acrylic = true,
+    Acrylic = false,
     Theme = "Ocean",
 })
+wait(0.1)
+
 l:Destroy()
 
-wait(5)
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/kawakihub/phkawaki/refs/heads/main/real-yobi/main/Grow.A.Garden/yobi/Kawakihub.lua"))()
-
-wait(5)
-
-game:GetService("StarterGui"):SetCore("SendNotification", {Title="Join My Discord Server", Text="https://discord.gg/49DAzBrkX4", Duration=5})
+wait(0.1)
+game:GetService("StarterGui"):SetCore("SendNotification", {Title="script loaded...", Text="Made by Kawaki", Duration=10})
